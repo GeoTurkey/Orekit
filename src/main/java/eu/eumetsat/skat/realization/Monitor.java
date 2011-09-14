60a9a59113a74b43ae2b760f83e24328e74abd0f
Luc Maisonobe <luc@orekit.org>
2011-09-14 13:47:18 +0200

    New Monitor interface (still empty yet)

---
+++ src/main/java/eu/eumetsat/skat/realization/Monitor.java
@@ -0,0 +1,13 @@
+/* Copyright 2011 Eumetsat */
+package eu.eumetsat.skat.realization;
+
+/**
+ * Interface for monitoring parameters evolution during station-keeping simulation
+ * @author Luc Maisonobe
+ */
+public interface Monitor {
+
+    /** Monitor regular evolution of a time-dependent value.
+     * @param 
+     */
+}
