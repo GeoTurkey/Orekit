c45baaef9ffcdad7e72222935f0a64473245f69b
Luc Maisonobe <luc@orekit.org>
2011-10-28 11:29:32 +0200

    Prepared new scenario component for some specific tasks.

---
+++ src/main/java/eu/eumetsat/skat/realization/LongManeuverEfficiencyLoss.java
@@ -0,0 +1,22 @@
+/* Copyright 2011 Eumetsat */
+package eu.eumetsat.skat.realization;
+
+import org.orekit.errors.OrekitException;
+
+import eu.eumetsat.skat.scenario.ScenarioComponent;
+import eu.eumetsat.skat.scenario.ScenarioState;
+
+/**
+ * Class for computing maneuver efficiency loss.
+ * @author Luc Maisonobe
+ */
+public class LongManeuverEfficiencyLoss implements ScenarioComponent {
+
+    /** {@inheritDoc} */
+    public ScenarioState[] updateStates(final ScenarioState[] originals)
+        throws OrekitException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
