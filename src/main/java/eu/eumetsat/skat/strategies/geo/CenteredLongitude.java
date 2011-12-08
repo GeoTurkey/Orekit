3306077491a2ec219c439ffdebb69c1479ebdbc6
Luc Maisonobe <luc@orekit.org>
2011-12-08 10:14:29 +0100

    Typo.

--- src/main/java/eu/eumetsat/skat/strategies/geo/CenteredLongitude.java
+++ src/main/java/eu/eumetsat/skat/strategies/geo/CenteredLongitude.java
@@ -31,7 +31,7 @@ import eu.eumetsat.skat.utils.SkatMessages;
  * <pre>
  *   max(|l<sub>75</sub> - l<sub>c</sub>|,|l<sub>c</sub> - l<sub>25</sub>|)
  * </pre>
- * where l<sub>75</sub> and l<sub>25</sub> are the spacecraft longitude quantities
+ * where l<sub>75</sub> and l<sub>25</sub> are the spacecraft longitude quantiles
  * at 75% and 25% evaluated for the complete cycle duration and l<sub>c</sub> is
  * the center longitude.
  * </p>
