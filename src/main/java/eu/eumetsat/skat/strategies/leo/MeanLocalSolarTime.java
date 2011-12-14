1e75acbfffe03a4e3ef04bc39b41ff1643ea7bb9
rdicosta <romain.di-costanzo@c-s.fr>
2011-12-14 16:54:46 +0100

    Compute MLST with periodic step at initialization

--- src/main/java/eu/eumetsat/skat/strategies/leo/MeanLocalSolarTime.java
+++ src/main/java/eu/eumetsat/skat/strategies/leo/MeanLocalSolarTime.java
@@ -4,6 +4,11 @@ package eu.eumetsat.skat.strategies.leo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.math.analysis.UnivariateFunction;
+import org.apache.commons.math.analysis.solvers.BracketingNthOrderBrentSolver;
+import org.apache.commons.math.analysis.solvers.BrentSolver;
+import org.apache.commons.math.analysis.solvers.UnivariateRealSolverUtils;
+import org.apache.commons.math.exception.NoBracketingException;
 import org.apache.commons.math.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math.stat.descriptive.rank.Percentile;
 import org.apache.commons.math.util.FastMath;
@@ -11,6 +16,7 @@ import org.apache.commons.math.util.MathUtils;
 import org.orekit.bodies.BodyShape;
 import org.orekit.bodies.GeodeticPoint;
 import org.orekit.errors.OrekitException;
+import org.orekit.errors.PropagationException;
 import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
 import org.orekit.propagation.Propagator;
@@ -53,8 +59,6 @@ import eu.eumetsat.skat.strategies.ScheduledManeuver;
  */
 public class MeanLocalSolarTime extends AbstractSKControl {
 
-    /** Associated event detector. */
-    private final EventDetector eventDetector;
 
     /** Sampled offset to target solar time. */
     private final List<Double> sample;
@@ -67,6 +71,15 @@ public class MeanLocalSolarTime extends AbstractSKControl {
 
     /** Search start. */
     private AbsoluteDate searchStart;
+
+    /** Latitude at witch the local solar time will be checked*/
+    private double latitude;
+
+    private BodyShape earth;
+
+    private boolean ascending;
+
+    private GMODFrame gmod;
 
     /** Simple constructor.
      * @param name name of the control law
@@ -92,17 +105,68 @@ public class MeanLocalSolarTime extends AbstractSKControl {
         throws OrekitException {
         super(name, scalingDivisor, controlledName, controlledIndex, null, -1,
               0., minSolarTime, maxSolarTime);
-        this.eventDetector        = new Detector(checkInterval, 1.0, earth, latitude, ascending);
         this.sample               = new ArrayList<Double>();
         this.center               = solarTime;
         this.ignoredStartDuration = ignoredStartDuration;
+        this.latitude             = latitude;
+        this.earth                = earth;
+        this.ascending            = ascending;
+        this.gmod                 = new GMODFrame();
     }
 
     /** {@inheritDoc} */
     public void initializeRun(final ScheduledManeuver[] maneuvers,
-                              final Propagator propagator, AbsoluteDate start, AbsoluteDate end, int rollingCycles)
-        throws OrekitException {
+                              final Propagator propagator, 
+                              final AbsoluteDate start,
+                              final AbsoluteDate end,
+                              int rollingCycles) throws OrekitException {
         searchStart = start.shiftedBy(ignoredStartDuration);
+        sample.clear();
+
+        final double period = propagator.getInitialState().getKeplerianPeriod();
+
+        // Find the first root :
+        final double stepSize = period / 100;
+
+        double dt = findFirstRoot(searchStart, stepSize, propagator);
+        AbsoluteDate rootDate = searchStart.shiftedBy(dt);
+        storeSample(rootDate, propagator, sample);
+
+
+        // Find all other root from periodic guess :
+        while (rootDate.shiftedBy(period).compareTo(end) < 0d){
+            final AbsoluteDate guessDate = rootDate.shiftedBy(period);
+            dt = findEncounter(guessDate, end, stepSize, period / 8d, propagator);
+            if (Double.isNaN(dt)){
+                // Last solution has been found :
+                break;
+            }
+            // Re-center date for next guess
+            rootDate = guessDate.shiftedBy(dt);
+            // Store current point
+            storeSample(rootDate, propagator, sample);
+        }
+    }
+    
+    private void storeSample(AbsoluteDate rootDate, Propagator propagator, List<Double> sample) throws OrekitException {
+
+        if (rootDate.compareTo(searchStart) > 0) {
+            // we crossed the specified latitude in the expected direction
+            SpacecraftState s = propagator.propagate(rootDate);
+            // compute angle between Sun and spacecraft in the equatorial plane
+            final Frame gcrf =  FramesFactory.getGCRF();
+            final Vector3D spacecraftPos = s.getPVCoordinates(gcrf).getPosition();
+            final double time = rootDate.getComponents(TimeScalesFactory.getUTC()).getTime().getSecondsInDay();
+            final double gmst     = gmod.getMeanSiderealTime(s.getDate());
+            final double sunAlpha = gmst + FastMath.PI * (1 - time / (Constants.JULIAN_DAY * 0.5));
+            final double dAlpha = MathUtils.normalizeAngle(spacecraftPos.getAlpha() - sunAlpha, 0);
+
+            // convert the angle to solar time
+            final double achievedSolarTime = 12.0 * (1.0 + dAlpha / FastMath.PI);
+
+            checkLimits(achievedSolarTime);
+            sample.add(achievedSolarTime);
+        }     
     }
 
     /** {@inheritDoc} */
@@ -119,96 +183,160 @@ public class MeanLocalSolarTime extends AbstractSKControl {
 
     /** {@inheritDoc} */
     public EventDetector getEventDetector() {
-        return eventDetector;
+        return null;
     }
 
     /** {@inheritDoc} */
     public OrekitStepHandler getStepHandler() {
         return null;
     }
+    
+    
 
-    /** Inner class for events handling. */
-    private class Detector extends AbstractDetector {
+    /** 
+     * Find the first root to then be able to use a periodic step 
+     * @param searchStart search start
+     * @param stepSize step size to use
+     * @param propagator propagator
+     * @return the first root
+     * @throws PropagationException
+     */
+    private double findFirstRoot(AbsoluteDate searchStart, double stepSize, Propagator propagator) throws PropagationException {
+        final UnivariateFunction latitudeFunction = new LatitudeFunction(searchStart, latitude, propagator, earth);
+        double previous = latitudeFunction.value(0d);
+        boolean up = false;
+        double root = Double.NaN;
+        BrentSolver solver = new BrentSolver(1e-12);
+        int i = 1;
+        boolean found = false;
+        while (!found){
+            
+            double next = latitudeFunction.value(i * stepSize);
 
-        /** Serializable UID. */
-        private static final long serialVersionUID = -5023948730619118388L;
-
-        /** Earth model. */
-        private final BodyShape earth;
-
-        /** Latitude at which solar time should be computed. */
-        private final double latitude;
-
-        /** Indicator for solar time computation direction. */
-        private final boolean ascending;
-
-        /** GMOD Frame. */
-        private final GMODFrame gmod;
-
-        /** UTC time scale. */
-        private final TimeScale utc;
-
-
-        /** Simple constructor
-         * @param maxCheck maximum checking interval (s)
-         * @param threshold convergence threshold (s)
-         * @param earth Earth model
-         * @param latitude latitude at which solar time should be computed
-         * @param ascending if true, solar time is computed when crossing the
-         * @param targetSolarTime target solar time
-         * @exception OrekitException if the UTC-TAI correction cannot be loaded
-         */
-        public Detector(final double maxCheck, final double threshold,
-                        final BodyShape earth, final double latitude,
-                        final boolean ascending)
-            throws OrekitException {
-            super(maxCheck, threshold);
-            this.earth           = earth;
-            this.latitude        = latitude;
-            this.ascending       = ascending;
-            this.gmod            = new GMODFrame();
-            this.utc             = TimeScalesFactory.getUTC();
+            // Check satellite motion
+            if (next - previous > 0d){
+                up = true;
+            }else {
+                up = false;
+            }
+            
+            // Check if bracketing has been done
+            if ((previous * next <= 0d) && (up == ascending)){
+                root = solver.solve(100, latitudeFunction, (i - 1) * stepSize, i * stepSize);
+                // Root found
+                found = true;
+            }
+            previous = next;
+            i++;
         }
+        return root;        
+    }
+    
+    
+    /**
+     * Find the date at witch the latitude is crossed 
+     * @param guessDate guess date from a periodic step
+     * @param endDate maximal date not to overtake
+     * @param shift shift value used to evaluate the latitude function bracketing around the guess date  
+     * @param maxShift maximum value that the shift value can take
+     * @param propagator propagator used
+     * @return date at witch the satellite is crossing the latitude
+     * @throws OrekitException
+     * @throws NoBracketingException
+     */
+    private double findEncounter(final AbsoluteDate guessDate, 
+                                 final AbsoluteDate endDate,
+                                 final double shift,
+                                 final double maxShift,
+                                 final Propagator propagator)
+        throws OrekitException, NoBracketingException {
 
-        /** {@inheritDoc} */
-        public void init(final SpacecraftState s0, final AbsoluteDate t) {
-            resetLimitsChecks();
-            sample.clear();
+        // Latitude function
+        final UnivariateFunction latitudeFunction = new LatitudeFunction(guessDate, latitude, propagator, earth);
+        
+        // try to bracket the encounter
+        double span;
+        if (guessDate.shiftedBy(shift).compareTo(endDate) > 0d){
+            // Take a 1e-3 security margin
+            span = endDate.durationFrom(guessDate) - 1e-3;
+        }else {
+            span = shift;
         }
+        
+        while (!UnivariateRealSolverUtils.isBracketing(latitudeFunction, - span, span)) {
 
-        /** {@inheritDoc} */
-        public double g(final SpacecraftState s)
-            throws OrekitException {
-            final Vector3D position = s.getPVCoordinates(earth.getBodyFrame()).getPosition();
-            final GeodeticPoint gp  = earth.transform(position, earth.getBodyFrame(), s.getDate());
-            return gp.getLatitude() - latitude;
-        }
-
-        /** {@inheritDoc} */
-        public Action eventOccurred(final SpacecraftState s, final boolean increasing)
-            throws OrekitException {
-
-            if (increasing == ascending && s.getDate().compareTo(searchStart) > 0) {
-                // we crossed the specified latitude in the expected direction
-
-                // compute angle between Sun and spacecraft in the equatorial plane
-                final Frame gcrf =  FramesFactory.getGCRF();
-                final Vector3D spacecraftPos = s.getPVCoordinates(gcrf).getPosition();
-                final double time = s.getDate().getComponents(utc).getTime().getSecondsInDay();
-                final double gmst     = gmod.getMeanSiderealTime(s.getDate());
-                final double sunAlpha = gmst + FastMath.PI * (1 - time / (Constants.JULIAN_DAY * 0.5));
-                final double dAlpha = MathUtils.normalizeAngle(spacecraftPos.getAlpha() - sunAlpha, 0);
-
-                // convert the angle to solar time
-                final double achievedSolarTime = 12.0 * (1.0 + dAlpha / FastMath.PI);
-
-                checkLimits(achievedSolarTime);
-                sample.add(achievedSolarTime);
+            if (2 * span > maxShift) {
+                // let the Apache Commons Math exception be thrown
+                UnivariateRealSolverUtils.verifyBracketing(latitudeFunction, - span, span);
+            }else if (guessDate.shiftedBy(2 * span).compareTo(endDate) >= 1){
+                // Out of range :
+                return Double.NaN;
             }
 
-            // just continue propagation
-            return Action.CONTINUE;
+            // expand the search interval
+            span *= 2;
 
+        }
+
+        // find the encounter in the bracketed interval
+        final BracketingNthOrderBrentSolver solver = new BracketingNthOrderBrentSolver(0.1, 5);
+        return solver.solve(1000, latitudeFunction, - span, span);
+
+    }
+    
+    
+    /**
+     * Function used to detect satellite latitude crossing
+     */
+    protected class LatitudeFunction implements UnivariateFunction{
+        
+        /** Propagator */
+        private Propagator propagator;
+        
+        /** Latitude */
+        private double latitude;
+        
+        /** Reference date used for computation*/
+        private AbsoluteDate referenceDate;
+        
+        /** Body shape */
+        private BodyShape earth;
+
+        /**
+         * Latitude function
+         * @param referenceDate date of reference
+         * @param latitude latitude wanted
+         * @param propagator propagator used 
+         * @param earth body shape
+         */
+        private LatitudeFunction(final AbsoluteDate referenceDate,
+                                 final double latitude,
+                                 final Propagator propagator,
+                                 final BodyShape earth){
+            this.latitude = latitude;
+            this.propagator = propagator;
+            this.referenceDate = referenceDate;
+            this.earth = earth;
+        }        
+
+
+        /**
+         * {@inheritDoc}
+         */
+        public double value(double x) {
+            SpacecraftState state;
+            double res = 0d;
+            try {
+                state = propagator.propagate(referenceDate.shiftedBy(x));
+                Vector3D position = state.getPVCoordinates(earth.getBodyFrame()).getPosition();
+                GeodeticPoint point = earth.transform(position, earth.getBodyFrame(), state.getDate());
+                res = point.getLatitude() - latitude;
+            } catch (PropagationException e) {
+                e.printStackTrace();
+            } catch (OrekitException e) {
+                e.printStackTrace();
+            }
+            return res;
         }
     }
 }
